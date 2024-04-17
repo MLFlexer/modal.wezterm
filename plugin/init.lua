5fec37310b6607cba5a3885d34878b80d18d2be2
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2024-04-17 20:27:42 +0200

    add get_mode_formatted

--- plugin/init.lua
+++ plugin/init.lua
@@ -20,4 +20,34 @@ end
 local function add_mode(name, key_table, color)
 	table.insert(modes, { name = name, key_table = key_table, color = color })
 end
+
 ---@param window any
+---@param formatter? fun(mode: mode): string
+local function get_mode_formatted(window, formatter)
+	local mode = modes[get_mode(window)]
+	if mode then
+		local status
+
+		-- checks if formatter is nil
+		if formatter then
+			status = formatter(mode)
+		else
+			status = wezterm.format({
+				{ Foreground = { Color = mode.fg_color } },
+				{ Background = { Color = mode.bg_color } },
+				{ Text = wezterm.nerdfonts.ple_left_half_circle_thick },
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { Color = mode.bg_color } },
+				{ Background = { Color = mode.fg_color } },
+				{ Text = mode.name .. "  " },
+			})
+		end
+		return status
+	end
+end
+
+return {
+	get_mode_formatted = get_mode_formatted,
+	add_mode = add_mode,
+	get_mode = get_mode,
+}
